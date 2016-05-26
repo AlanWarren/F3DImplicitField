3280e8079ffc3727a4df83d122392a3307a58f40
Alan Warren <alanw@televox.com>
2016-05-26 11:56:38 -0500

    adding license

--- f3d2prman.cpp
+++ f3d2prman.cpp
@@ -1,7 +1,21 @@
-/* f3d2prman
- * author: Alan Warren <email@alan-warren.com>
- * comments: This is a runprogram for F3DImplicitField
- */
+/*  f3d2prman
+ *  author: Alan Warren <william.alan.warren@gmail.com>
+ *  comments: This is a runprogram for F3DImplicitField
+ *  Copyright (C) 2016  William A. Warren
+
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <iostream>
 #include <string>
